e3e41bcde0840783b8293087970d7ab53b30608e
Corey O'Connor <coreyoconnor@gmail.com>
2020-05-22 23:42:58 -0700

    ~

--- modules/retroarch.nix
+++ modules/retroarch.nix
@@ -83,13 +83,12 @@ in
         enableBeetleSuperGrafx = true;
         enableBeetleSaturn = true;
         enableBsnesMercury = true;
+        enableGenesisPlusGX = true;
         enableMBGA = true;
-        # enableMupen64Plus = true;
         enableParallelN64 = true;
-        # enableNestopia = true;
+        enablePCSXRearmed = true;
         enableQuickNES = true;
         enableSnes9x = true;
-        # enableSnes9xNext = true;
         enableVbaM = true;
       };
     };
