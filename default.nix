3942c2cf136572bccb3abe0ad3344dc27570ec97
Corey O'Connor <coreyoconnor@gmail.com>
2023-10-20 17:42:23 -0700

    update sha256

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let cfg = config.retronix;
     cmd-on-event = (import cmd-on-event-src) {
       inherit pkgs;
       src = cmd-on-event-src;
-      outputHash = "sha256-Vl4hogum9/+73xkzrQnPzinzbbe9HTP7JabvuWnEnQ0=";
+      outputHash = "sha256-o5gqU0RX4nJNQ4UKFGwafUJKBElF0j596kfkWfnv8aU=";
     };
     restart-display-manager = "/run/current-system/sw/bin/systemctl restart display-manager.service";
 in {
